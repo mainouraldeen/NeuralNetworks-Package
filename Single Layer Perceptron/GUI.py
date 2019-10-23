eebe29a7bd8bd9a551dec47f1ad19f66a54c2ea3
Mai Nour Al-Deen <mai.mohamed339@gmail.com>
2019-10-23 02:39:54 +0200

    draw Iris data function.

--- Single Layer Perceptron/GUI.py
+++ Single Layer Perceptron/GUI.py
@@ -2,103 +2,133 @@ from tkinter import *
 from tkinter import messagebox
 import Model
 
-
-top =Tk()
+top = Tk()
 top.geometry("1000x500")
+
+
 ########################################################################
-#labels
-var= StringVar()
-l1=Label(top, textvariable=var)
+# Functions
+def collectData():
+    feature1 = feature1var.get()
+    feature2 = feature2var.get()
+    class1 = class1var.get()
+    class2 = class2var.get()
+    learningRate = LearningRatetextBox.get()
+    epochs = EpochstextBox.get()
+    bias = biasvar.get()
+    '''
+    if (class1 == "Iris-setosa"):
+        class1 = "001"
+    if (class1 == "Iris-versicolor"):
+        class1 = "010"
+    if (class1 == "Iris-virginica"):
+        class1 = "100"
+
+    if (class2 == "Iris-setosa"):
+        class2 = "001"
+    if (class2 == "Iris-versicolor"):
+        class2 = "010"
+    if (class2 == "Iris-virginica"):
+        class2 = "100" 
+    '''
+    print(feature1, feature2, class1, class2, learningRate, epochs, bias)
+
+
+########################################################################
+# labels
+var = StringVar()
+l1 = Label(top, textvariable=var)
 var.set("Feature 1")
-l1.place(x=50,y=50)
+l1.place(x=50, y=50)
 
-var= StringVar()
-l2=Label(top, textvariable=var)
+var = StringVar()
+l2 = Label(top, textvariable=var)
 var.set("Feature 2")
-l2.place(x=400,y=50)
+l2.place(x=400, y=50)
 
-var= StringVar()
-l3=Label(top, textvariable=var)
+var = StringVar()
+l3 = Label(top, textvariable=var)
 var.set("Class 1")
-l3.place(x=50,y=200)
+l3.place(x=50, y=200)
 
-var= StringVar()
-l4=Label(top, textvariable=var)
+var = StringVar()
+l4 = Label(top, textvariable=var)
 var.set("Class 2")
-l4.place(x=400,y=200)
+l4.place(x=400, y=200)
 
-var= StringVar()
-l5=Label(top, textvariable=var)
+var = StringVar()
+l5 = Label(top, textvariable=var)
 var.set("Learning Rate")
-l5.place(x=50,y=350)
+l5.place(x=50, y=350)
 
-var= StringVar()
-l6=Label(top, textvariable=var)
+var = StringVar()
+l6 = Label(top, textvariable=var)
 var.set("Epochs")
-l6.place(x=400,y=350)
+l6.place(x=400, y=350)
 
-var= StringVar()
-l7=Label(top, textvariable=var)
+var = StringVar()
+l7 = Label(top, textvariable=var)
 var.set("Bias")
-l7.place(x=700,y=350)
+l7.place(x=700, y=350)
 
 ########################################################################
-#Buttons
-drawButton=Button(top, text="Draw Iris Data",command = Model.DrawIrisData)
-drawButton.place(x=200,y=450)
+# Buttons
+drawButton = Button(top, text="Draw Iris Data", command=Model.DrawData)
+drawButton.place(x=200, y=450)
 
-runButton=Button(top, text="Run",command = Model.perceptron)
-runButton.place(x=600,y=450)
+runButton = Button(top, text="Run", command=collectData)
+runButton.place(x=600, y=450)
 
 ########################################################################
-#TextBoxs
+# TextBoxs
 LearningRatetextBox = Entry(top)
-LearningRatetextBox.place(x=140,y=350)
+LearningRatetextBox.place(x=140, y=350)
+LearningRatetextBox.focus_set()
 
 EpochstextBox = Entry(top)
-EpochstextBox.place(x=480,y=350)
+EpochstextBox.place(x=480, y=350)
 
 ########################################################################
-#DropBox
+# DropBox
 feature1DropBox = Frame(top)
-feature1DropBox.place(y = 50, x = 120)
-var = StringVar(top)
-choices = { 'X1','X2','X3','X4'}
-var.set('X1') # set the default option
-popupMenu = OptionMenu(feature1DropBox, var, *choices)
-popupMenu.grid(row = 2, column =1)
+feature1DropBox.place(y=50, x=120)
+feature1var = StringVar(top)
+choices = {'X1', 'X2', 'X3', 'X4'}
+feature1var.set('X1')  # set the default option
+popupMenu = OptionMenu(feature1DropBox, feature1var, *choices)
+popupMenu.grid(row=2, column=1)
 
 feature2DropBox = Frame(top)
-feature2DropBox.place(y = 50, x = 500)
-var = StringVar(top)
-choices = { 'X1','X2','X3','X4'}
-var.set('X1') # set the default option
-popupMenu = OptionMenu(feature2DropBox, var, *choices)
-popupMenu.grid(row = 2, column =1)
+feature2DropBox.place(y=50, x=500)
+feature2var = StringVar(top)
+choices = {'X1', 'X2', 'X3', 'X4'}
+feature2var.set('X1')  # set the default option
+popupMenu = OptionMenu(feature2DropBox, feature2var, *choices)
+popupMenu.grid(row=2, column=1)
 
 class1DropBox = Frame(top)
-class1DropBox.place(y = 200, x = 120)
-var = StringVar(top)
-choices = { 'Iris-setosa','Iris-versicolor','Iris-virginica'}
-var.set('Iris-setosa') # set the default option
-popupMenu = OptionMenu(class1DropBox, var, *choices)
-popupMenu.grid(row = 2, column =1)
+class1DropBox.place(y=200, x=120)
+class1var = StringVar(top)
+choices = {'Iris-setosa', 'Iris-versicolor', 'Iris-virginica'}
+class1var.set('Iris-setosa')  # set the default option
+popupMenu = OptionMenu(class1DropBox, class1var, *choices)
+popupMenu.grid(row=2, column=1)
 
 class2DropBox = Frame(top)
-class2DropBox.place(y = 200, x = 500)
-var = StringVar(top)
-choices = { 'Iris-setosa','Iris-versicolor','Iris-virginica'}
-var.set('Iris-setosa') # set the default option
-popupMenu = OptionMenu(class2DropBox, var, *choices)
-popupMenu.grid(row = 2, column =1)
+class2DropBox.place(y=200, x=500)
+class2var = StringVar(top)
+choices = {'Iris-setosa', 'Iris-versicolor', 'Iris-virginica'}
+class2var.set('Iris-setosa')  # set the default option
+popupMenu = OptionMenu(class2DropBox, class2var, *choices)
+popupMenu.grid(row=2, column=1)
 
 biasDropBox = Frame(top)
-biasDropBox.place(y = 350, x = 750)
-var = StringVar(top)
-choices = { '0','1'}
-var.set('1') # set the default option
-popupMenu = OptionMenu(biasDropBox, var, *choices)
-popupMenu.grid(row = 2, column =1)
+biasDropBox.place(y=350, x=750)
+biasvar = StringVar(top)
+choices = {'0', '1'}
+biasvar.set('1')  # set the default option
+popupMenu = OptionMenu(biasDropBox, biasvar, *choices)
+popupMenu.grid(row=2, column=1)
 ########################################################################
 
 
--- Single Layer Perceptron/Model.py
+++ Single Layer Perceptron/Model.py
@@ -1,36 +1,8 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
 from sklearn import preprocessing
-
-
-def DrawIrisData():
-    print("doneeeeeeeeeeeeeeee gui")
-
-
-def perceptron():
-    print("doneeeeeeeeeeeeeeee algo")
-
-
-def signum(prediction):
-    return 1 if prediction > 0 else -1
-
-    # 3ndy vector ll weights l kol layer msh l kol input
-    # w ana 3ndy one layer: only the output layer
-    # current layer: output layer (only one neuron)
-def perceptron_model(X, Y, alpha, epochs):  # assume Y: numbers not words
-
-    w = np.random.rand(1, 2)  # W[#neurons of the current layer, #neurons of the previous layer]
-    b = np.zeros(1, 1)  # b[#neurons of the current layer, 1]
-
-    for i in range(epochs):
-        # prediction = np.dot(w1, X1) + np.dot(w2, X2) + b  # ?np.dot wala *
-
-        prediction = np.dot(w, X) + b  # ?np.dot wala *
-        yHat = signum(prediction)  # ?values of signum
-        error = Y - yHat  # ? y hat victor wala la
-        w = w + (alpha * error * X)  # anhy x ??
-        b = b + (alpha * error)  # adrb f eh!
-    return w, b
 
 
 def load_data(data):
@@ -77,12 +49,119 @@ def load_data(data):
     return X1_train, X1_test, X2_train, X2_test, X3_train, X3_test, X4_train, X4_test, Y_train, Y_test
 
 
+data = pd.read_csv('IrisData.csv')
+X1_train, X1_test, X2_train, X2_test, X3_train, X3_test, X4_train, X4_test, target_train, target_test = load_data(
+    data)
+
+le = preprocessing.LabelEncoder()
+le.fit(target_train)
+le.fit(target_test)
+labeled_Y_train = le.transform(target_train)
+labeled_Y_test = le.transform(target_test)
+
+
+def DrawIrisData():
+    # x1,X2
+    x1 = X1_train.append(X1_test)
+    x2 = X2_train.append(X2_test)
+    label = np.append(labeled_Y_train, labeled_Y_test)
+    colors = ['grey', 'blue', 'purple']
+
+    plt.scatter(x1, x2, c=label, cmap=matplotlib.colors.ListedColormap(colors))
+    plt.xlabel('X1', fontsize=20)
+    plt.ylabel('X2', fontsize=20)
+    plt._show()
+
+    # x1,X3
+    x1 = X1_train.append(X1_test)
+    x3 = X3_train.append(X3_test)
+    label = np.append(labeled_Y_train, labeled_Y_test)
+    colors = ['grey', 'blue', 'purple']
+
+    plt.scatter(x1, x3, c=label, cmap=matplotlib.colors.ListedColormap(colors))
+    plt.xlabel('X1', fontsize=20)
+    plt.ylabel('X3', fontsize=20)
+    plt._show()
+
+    # x1,X4
+    x1 = X1_train.append(X1_test)
+    x4 = X4_train.append(X4_test)
+    label = np.append(labeled_Y_train, labeled_Y_test)
+    colors = ['grey', 'blue', 'purple']
+
+    plt.scatter(x1, x4, c=label, cmap=matplotlib.colors.ListedColormap(colors))
+    plt.xlabel('X1', fontsize=20)
+    plt.ylabel('X4', fontsize=20)
+    plt._show()
+
+    # x2,X3
+    x2 = X2_train.append(X2_test)
+    x3 = X3_train.append(X3_test)
+    label = np.append(labeled_Y_train, labeled_Y_test)
+    colors = ['grey', 'blue', 'purple']
+
+    plt.scatter(x2, x3, c=label, cmap=matplotlib.colors.ListedColormap(colors))
+    plt.xlabel('X2', fontsize=20)
+    plt.ylabel('X3', fontsize=20)
+    plt._show()
+
+    # x2,X4
+    x2 = X2_train.append(X2_test)
+    x4 = X4_train.append(X4_test)
+    label = np.append(labeled_Y_train, labeled_Y_test)
+    colors = ['grey', 'blue', 'purple']
+
+    plt.scatter(x2, x4, c=label, cmap=matplotlib.colors.ListedColormap(colors))
+    plt.xlabel('X2', fontsize=20)
+    plt.ylabel('X4', fontsize=20)
+    plt._show()
+
+    # x3,X4
+    x3 = X3_train.append(X3_test)
+    x4 = X4_train.append(X4_test)
+    label = np.append(labeled_Y_train, labeled_Y_test)
+    colors = ['grey', 'blue', 'purple']
+
+    plt.scatter(x3, x4, c=label, cmap=matplotlib.colors.ListedColormap(colors))
+    plt.xlabel('X3', fontsize=20)
+    plt.ylabel('X4', fontsize=20)
+    plt._show()
+
+
+def perceptron():
+    print("doneeeeeeeeeeeeeeee algo")
+
+
+def signum(prediction):
+    return 1 if prediction > 0 else -1
+
+    # 3ndy vector ll weights l kol layer msh l kol input
+    # w ana 3ndy one layer: only the output layer
+    # current layer: output layer (only one neuron)
+
+
+def perceptron_model(X, Y, alpha, epochs):  # assume Y: numbers not words
+
+    w = np.random.rand(1, 2)  # W[#neurons of the current layer, #neurons of the previous layer]
+    b = np.zeros(1, 1)  # b[#neurons of the current layer, 1]
+
+    for i in range(epochs):
+        # prediction = np.dot(w1, X1) + np.dot(w2, X2) + b  # ?np.dot wala *
+
+        prediction = np.dot(w, X) + b  # ?np.dot wala *
+        yHat = signum(prediction)  # ?values of signum
+        error = Y - yHat  # ? y hat victor wala la
+        w = w + (alpha * error * X)  # anhy x ??
+        b = b + (alpha * error)  # adrb f eh!
+    return w, b
+
+
+def DrawData():
+    DrawIrisData()
+
+
 def main():
-    data = pd.read_csv('IrisData.csv')
-
-    X1_train, X1_test, X2_train, X2_test, X3_train, X3_test, X4_train, X4_test, target_train, target_test = load_data(data)
-
-
+    '''
     le = preprocessing.LabelEncoder()
     le.fit(target_train)
     le.fit(target_test)
@@ -97,10 +176,10 @@ def main():
         W, b = perceptron_model([X1_train[i], X2_train[i]], labeled_Y_train[i], alpha, epochs)
 
     # msh 3arfa hn-test ezay, hnb3t X_train ezay? element element wala vector??
+    '''
 
 
-#main()
-
+main()
 
 '''# Loading data
 inputFile = open("IrisData.txt", "r+")
